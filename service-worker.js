e06ca7fd5b5388c76704bf51eb5a67ba7d487ec2
armandomanthos <armando@manthos.nl>
2026-08-30 17:58:36 +0200

    Cache branded splash and refreshed dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE = 'manthos-habits-v3-4-10-dashboard';
+const CACHE = 'manthos-habits-v3-4-11-brand-assets';
 const ASSETS = [
-  './','./index.html','./manifest.json','./v33-content.css','./v33-content.js','./v33-nav.js','./v331-install.css','./v331-install.js','./v34-core.css','./v34-core.js','./v34-fixes.js','./v34-hub.css','./v34-hub.js','./v34-paths.css','./v34-paths.js','./assets/archetypes/Krijger.png','./assets/archetypes/Hoeder.png','./assets/archetypes/Vakman.png','./assets/navigation/Dashboard.png','./assets/navigation/Manthos-nu.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'
+  './','./index.html','./manifest.json','./v33-content.css','./v33-content.js','./v33-nav.js','./v331-install.css','./v331-install.js','./v34-core.css','./v34-core.js','./v34-fixes.js','./v34-hub.css','./v34-hub.js','./v34-paths.css','./v34-paths.js','./assets/archetypes/Krijger.png','./assets/archetypes/Hoeder.png','./assets/archetypes/Vakman.png','./assets/navigation/Dashboard.png','./assets/navigation/Splash screen.png','./assets/navigation/Manthos-nu.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
